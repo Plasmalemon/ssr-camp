c5075f991862ae163239789719fec6db0bc720b9
plasmalemon <781888977@qq.com>
2020-06-14 22:02:39 +0800

    feat: 增加异步store到client 和 server  未完成首页异步数据

---
+++ mock.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/api/course/list', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+    res.header('Content-Type', "application/json;charset=utf-8")
+    res.json({
+        code: 0,
+        list: [
+            { name: '1', id: 1 },
+            { name: '2', id: 2 },
+            { name: '3', id: 3 },
+            { name: '4', id: 4 },
+        ]
+    })
+})
+
+app.listen(9090, () => {
+    console.log('mock 启动完毕')
+})
--- src/container/Index.js
+++ src/container/Index.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { getIndexList } from '../store/index'
 
 function Index(props) {
     const [count, setCount] = useState(1)
+
+    useEffect(() => {
+        props.getIndexList()
+    }, []);
+
     return <div>
         <h1>hello,{props.title}</h1>
 
         <button onClick={() => setCount(count + 1)}>{count}</button>
+        <hr />
+
+        <ul>
+            {
+                props.list.map(item => <li key={item.id}>{item.name}</li>)
+            }
+        </ul>
     </div>
 }
 
-export default Index
+export default connect(
+    state => ({ list: state.index.list }),
+    { getIndexList }
+)(Index)
